8a5861d8fdc07d095b05d184e74886b300493093
JuvitaSaini09 <juvitasaini@gmail.com>
2021-08-27 20:41:35 +0530

    docS:document updated

--- script.js
+++ script.js
@@ -1,33 +1,52 @@
 const dob=document.querySelector("#dob");
 const luckyNo=document.querySelector("#lucky-no");
-const checkButton=document.querySelector("#check-button");
 const errorMessage=document.querySelector("#error-message");
+const checkButton=document.querySelector(".check-button");
 
 function OnClickHandler(){
-    console.log("hi");
-}
-    /*
     hideError();
     const dateOfBirth=dob.value;
     const luckyNumber=luckyNo.value;
     if(dateOfBirth&&luckyNumber)
-    {
+    { 
+       calculateLuckyNumber();
 
     }
     else{
-        showError("Enter valid Values !!");
-    }
-    function hideError()
+        
+        showError("Enter valid Value !!");
+ }
+}
+function calculateLuckyNumber(){
+
+  const Bdate=dob.value.replaceAll("-","");
+  const LuckyNo=luckyNo.value;
+  console.log(typeof Bdate);
+  const Sum=0;
+  /*
+  for(let i=0;i<Bdate.length;i++){
+    
+       Sum=Sum+(Bdate.CharAt(i));
+  }
+console.log(Sum);
+*/
+}
+function hideError()
 {
   errorMessage.style.display="none";
 }
-function showError()
+
+function showError(ErrorMessageDisplay)
 {
+  
   errorMessage.style.display="block";
-
-    */
-
+  errorMessage.innerText=ErrorMessageDisplay;
 
 
+}
 
 checkButton.addEventListener("click",OnClickHandler);
+    
+
+
+
